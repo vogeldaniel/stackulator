03adc469fe00ff539d1f504c29299b158ebe518d
Daniel <vogdan20@evergreen.edu>
2019-08-12 19:22:11 -0700

    Updates

---
+++ precache-manifest.37972e310bc6f08bc55a44c42fb9c484.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a2f0103b1f4477b7f175ee20e5fc13a",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "d7952a2a0db3cef96bca",
+    "url": "/react-gh-pages/static/css/main.089a0695.chunk.css"
+  },
+  {
+    "revision": "15ba4249a093b4cfe339",
+    "url": "/react-gh-pages/static/js/2.154e43df.chunk.js"
+  },
+  {
+    "revision": "d7952a2a0db3cef96bca",
+    "url": "/react-gh-pages/static/js/main.ebd84fc6.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  }
+]);
