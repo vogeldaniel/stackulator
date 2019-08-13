bd7f147944ca31ed74189d4271ce84ce0af5d1db
Daniel <vogdan20@evergreen.edu>
2019-08-12 19:23:35 -0700

    Updates

---
+++ precache-manifest.406176d6890876b644459474b07624c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f797dcaa637e7a60a88aab98eea6b007",
+    "url": "/stackulator/index.html"
+  },
+  {
+    "revision": "d97c20294f2170a6ec45",
+    "url": "/stackulator/static/css/main.089a0695.chunk.css"
+  },
+  {
+    "revision": "adf57bf48024e585a227",
+    "url": "/stackulator/static/js/2.302864a0.chunk.js"
+  },
+  {
+    "revision": "d97c20294f2170a6ec45",
+    "url": "/stackulator/static/js/main.5524d360.chunk.js"
+  },
+  {
+    "revision": "9c84ca677bbac1de15eb",
+    "url": "/stackulator/static/js/runtime~main.db72c1ba.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.37972e310bc6f08bc55a44c42fb9c484.js"
+  "/stackulator/precache-manifest.406176d6890876b644459474b07624c1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/stackulator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
